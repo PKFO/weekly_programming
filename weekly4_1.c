31fc7e339046954cb4a216316f6b77206a8aac9b
perawat_kosit <perawat246@gmail.com>
2022-09-04 15:24:33 +0700

    add file

---
+++ weekly4_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a[2];
+    scanf("%d %d",&a[1],&a[2]);
+    printf("%d\n",a[1] + a[2]);
+    printf("%d\n",a[1] - a[2]);
+    printf("%d\n",a[1] * a[2]);
+    printf("%d\n",a[1] / a[2]);
+    return 0;
+}
