915ec2f2767cac39348c532c82e4cc1931b3d5e1
PKFO <110986886+PKFO@users.noreply.github.com>
2022-10-30 14:47:52 +0700

    Update

--- game/Level.py
+++ game/Level.py
@@ -1,15 +1,23 @@
 import pygame
+from tile import Tile
 from map_settings import *
-class Level:
-    def _init_(Self):
+from player import Player
+class level:
+    def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
-
         self.create_map()
     def create_map(self):
-        for row in WORLD_MAP:
-            print(row)
-
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
     def run(self):
-        pass
+        #update and draw the game
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
