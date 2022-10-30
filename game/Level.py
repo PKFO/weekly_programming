df77fd59efbf59c79104f631b97b2579e1fe2905
PKFO <110986886+PKFO@users.noreply.github.com>
2022-10-30 14:48:17 +0700

    Update

--- game/Level.py
+++ game/Level.py
@@ -1,23 +1,54 @@
 import pygame
-from tile import Tile
 from map_settings import *
-from player import Player
-class level:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
-        self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
-        self.create_map()
-    def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
-    def run(self):
-        #update and draw the game
-        self.visible_sprites.draw(self.display_surface)
-        self.visible_sprites.update()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,groups,obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load("C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/character/player.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+        self.obstacle_sprites = obstacle_sprites
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+    def move(self,speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')   
+        #self.rect.center += self.direction * speed
+    def collision(self,direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: #moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+        if direction == 'vertical':
+             for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0: #moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+    def update(self):
+        self.input()
+        self.move(self.speed)
