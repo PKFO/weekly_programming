56236d0d3e4187cc51c2830ea969e15d47ec275e
PKFO <110986886+PKFO@users.noreply.github.com>
2022-11-07 02:08:03 +0700

    Update

--- game/Level.py
+++ game/Level.py
@@ -3,6 +3,7 @@ import pygame
 from tile import Tile
 from map_settings import *
 from player import Player
+from support import *
 class level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
@@ -10,14 +11,24 @@ class level:
         self.obstacles_sprites = pygame.sprite.Group()
         self.create_map()
     def create_map(self):
-        # for row_index,row in enumerate(WORLD_MAP):
-        #     for col_index,col in enumerate(row):
-        #         x = col_index * TILESIZE
-        #         y = row_index * TILESIZE
-        #         if col == 'x':
-        #             Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
-        #         if col == 'p':
-        #             self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
+        layout = {
+             'boudary' :import_csv_layout('C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/graphic/gameproject_Floorblock_Floorblock.csv'),
+             'detail' : import_csv_layout('C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/graphic/gameproject_Floorblock_detail.csv')
+         }
+        for style,layout in layout.items():
+            for row_index,row in enumerate(layout):
+                for col_index,col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boudary':
+                            Tile((x,y),[self.obstacles_sprites],'invisible')
+                        if style == 'detail':
+                            Tile((x,y),[self.obstacles_sprites],'invisible')
+        #          if col == 'x':
+        #              Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+        #          if col == 'p':
+        #              self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
         self.player = Player((2000,1500),[self.visible_sprites],self.obstacles_sprites)
     def run(self):
         #update and draw the game
@@ -31,13 +42,13 @@ class camera(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
-        self.floor_surface = pygame.image.load('C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/graphic/floor.png')
+        self.floor_surface = pygame.image.load('C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/graphic/floor.png').convert()
         self.floor_rect =   self.floor_surface.get_rect(topleft = (0,0))
     def custom_draw(self,player):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
         floor_offset_pos = self.floor_rect.topleft - self.offset
         self.display_surface.blit(self.floor_surface,floor_offset_pos)
-        for sprite in self.sprites():
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image,offset_pos) 
