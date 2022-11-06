f75e7ca6995bc84588c9e25dc4dbdd66ecbd67f6
PKFO <110986886+PKFO@users.noreply.github.com>
2022-11-07 02:07:30 +0700

    Update

--- game/player.py
+++ game/player.py
@@ -1,30 +1,65 @@
 import pygame
 from map_settings import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load("C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/character/Faceset.png").convert_alpha()
+        self.image = pygame.image.load("C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/character/Player1.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-26)
+
+        self.import_player_assets()
+        self.status = 'stand'
+        self.frame_index = 0
+        self.animation_speed = 0.15
+
         self.direction = pygame.math.Vector2()
-        self.speed = 5
+        self.speed = 3
         self.obstacle_sprites = obstacle_sprites
+    def import_player_assets(self):
+        character_path = 'C:/Users/Admin/Desktop/งานทุกวิชา/เขียนโค้ด/Pro_fun/game/character/player/'
+        self.animations = {'stand_idle':[],'walk':[],'walk_left':[],'walk_right':[],'back':[],'right_idle':[],'left_idle':[],'back_idle':[]}
+    
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+            print(self.animations)
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'back'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'stand'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         else:
             self.direction.x = 0
+        
+        #attack
+    def get_status(self):
+
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not 'idle' in self.status:
+                self.status = self.status + '_idle'
+    def animate(self):
+        animation = self.animations[self.status]
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
     def move(self,speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
@@ -52,3 +87,5 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.move(self.speed)
+        self.get_status()
+        self.animate()
